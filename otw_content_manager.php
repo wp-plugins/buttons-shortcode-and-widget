4b04464458c5bd9a305f35132a179268f2334236
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-28 14:54:04 +0000

    version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/buttons-shortcode-and-widget/trunk@922542 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- otw_content_manager.php
+++ otw_content_manager.php
@@ -4,7 +4,7 @@ Plugin Name: Buttons Shortcode And Widgets
 Plugin URI: http://OTWthemes.com
 Description:  Create buttons. Nice and easy interface. Insert anywhere in your site - page/post editor, sidebars, template files. 
 Author: OTWthemes.com
-Version: 1.1
+Version: 1.2
 
 Author URI: http://themeforest.net/user/OTWthemes
 */
